2efae8abf40d9a6a62f3d385a54e4b16b3b16026
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 17:40:59 +0300

    step-16

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // позиция коробля
-const randomLoc = Math.floor(Math.random() * 5)  // от 0 до 4 генерация рандомного числа 
-const location1 = randomLoc                      //  Math.random() * 5 генерирует дробное число кроме 5
-const location2 = location1 + 1                  //  Math.floor() отсекает дробную часть
-const location3 = location2 + 1
+const randomLoc = Math.floor(Math.random() * 5); // от 0 до 4 генерация рандомного числа
+const location1 = randomLoc; //  Math.random() * 5 генерирует дробное число кроме 5
+const location2 = location1 + 1; //  Math.floor() отсекает дробную часть
+const location3 = location2 + 1;
 
 // потоплен или нет
 let isSunk = false;
@@ -10,6 +10,8 @@ let isSunk = false;
 let guess;
 // дабавим счётчик попаданий
 let hits = 0;
+// создаём счётчик выстрелов
+let guesses = 0;
 
 while (isSunk === false) {
   guess = +prompt("Fire! Enter a number 0-6"); // +prompt строковое значение превратим в число при помощи +
@@ -17,6 +19,8 @@ while (isSunk === false) {
   if (guess < 0 || guess > 6) {
     alert("Please, enter a valid cell number!");
   } else {
+    // увеличиваем счётчик выстрелов
+    guesses = guesses + 1;
     // лоигческое "или"
     if (guess === location1 || guess === location2 || guess === location3) {
       alert("HIT!");
@@ -32,3 +36,6 @@ while (isSunk === false) {
     }
   }
 }
+
+const stats = "You have " + hits + " hits out of " + guesses + " shots.";
+alert(stats);
