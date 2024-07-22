0d17a0bf5fa917a75efd20865a008b46a028dfd8
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 16:18:59 +0300

    step-6

--- main.js
+++ main.js
@@ -8,4 +8,9 @@ let guess;
 
 while (isSunk === false) {
     guess = +prompt("Fire! Enter a number 0-6") // +prompt строковое значение превратим в число при помощи +
+    if(guess === location1 || guess === location2 || guess === location3) {
+        alert("HIT!")
+    } else {
+        alert("MISS!")
+    }
 }
