0552db27ad5d1b331523fba411fdf15c97882580
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 16:23:01 +0300

    step-7

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+// позиция коробля
 const location1 = 3;
 const location2 = 4;
 const location3 = 5;
 
+// потоплен или нет
 let isSunk = false;
+// предполагаемоя позиция корабля ("выстрел")
 let guess;
 
 
 while (isSunk === false) {
     guess = +prompt("Fire! Enter a number 0-6") // +prompt строковое значение превратим в число при помощи +
+    // лоигческое "или"
     if(guess === location1 || guess === location2 || guess === location3) {
         alert("HIT!")
     } else {
