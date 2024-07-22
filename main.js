578d4f5dd47da9eb41f914c59eed23c48f760922
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-22 16:49:37 +0300

    step-12

--- main.js
+++ main.js
@@ -12,22 +12,21 @@ let hits = 0;
 
 while (isSunk === false) {
   guess = +prompt("Fire! Enter a number 0-6"); // +prompt строковое значение превратим в число при помощи +
-  if(guess < 0 || guess > 6){
-    alert("Please, enter a valid cell number!")
+  if (guess < 0 || guess > 6) {
+    alert("Please, enter a valid cell number!");
   } else {
-
-  }
-  // лоигческое "или"
-  if (guess === location1 || guess === location2 || guess === location3) {
-    alert("HIT!");
-    // считаем попадания
-    hits = hits + 1;
-    // если подбили
-    if (hits === 3) {
-      isSunk = true;
-      alert("You sank my battleship!");
+    // лоигческое "или"
+    if (guess === location1 || guess === location2 || guess === location3) {
+      alert("HIT!");
+      // считаем попадания
+      hits = hits + 1;
+      // если подбили
+      if (hits === 3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
+    } else {
+      alert("MISS!");
     }
-  } else {
-    alert("MISS!");
   }
 }
